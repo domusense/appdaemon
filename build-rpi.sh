f40626244713cbac202b5c547f3e1e3c679fdea0
domusense <37619939+domusense@users.noreply.github.com>
2018-06-02 08:09:21 +0300

    Update build-rpi.sh

--- build-rpi.sh
+++ build-rpi.sh
@@ -1,7 +1,24 @@
 docker build -f Dockerfile -t domu/appdaemon-rpi:latest .
 
+# User data volume 
+docker volume create --name appdaemon_user_data
+
+if [ $? -ne 0 ]; then
+    echo "Cannot create volume for user data."
+    exit 1
+fi
+
 docker run --rm -it -p 5050:5050 \
+-v appdaemon_user_data:/conf --name appdaemon \
 -e HA_URL="http://192.168.1.18:8123" \
 -e HA_KEY="12345678" \
 -e DASH_URL="http://192.168.1.18:5050" \
 domu/appdaemon-rpi:latest
+
+#docker run -d -p 5050:5050 \
+#-v appdaemon_user_data:/conf --name appdaemon \
+#-e HA_URL="http://192.168.1.18:8123" \
+#-e HA_KEY="12345678" \
+#-e DASH_URL="http://192.168.1.18:5050" \
+#domu/appdaemon-rpi:latest
+
